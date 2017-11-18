3ba32774a72381e4ee6c8ab7cebec5c47ae3dc85
basharal <basharal@amazon.com>
2017-11-18 11:31:32 +1100

    Added options

--- cli/generate.php
+++ cli/generate.php
@@ -4,11 +4,12 @@ $start = microtime(true);
 class cli { public static function log($m) { echo '[' . date('Y-m-d H:i:s') . '] ' . $m . PHP_EOL; } }
 require __DIR__ . '/../vendor/autoload.php';
 
-$shortopts = 'i:d::';
-$longopts = array('implementation:', 'dev::',);
+$shortopts = 'i:d::f::';
+$longopts = array('implementation:', 'dev::', 'file::',);
 $opts = getopt($shortopts, $longopts);
 $isDev = isset($opts['d']) ? $opts['d'] : true;
 $implementation = isset($opts['i']) ? $opts['i'] : 'file';
+$file = isset($opts['f']) ? $opts['f'] : sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'dataset.json';
 
 $json = array();
 if (!$isDev) { 
@@ -31,6 +32,7 @@ $kinesis = Aws\Kinesis\KinesisClient::factory(array(
     'version' => 'latest',
 ));
 
+$msg = null;
 switch ($implementation) { 
     case 'kinesis':
         $streamName = 'elasticsearch-stream-01';
@@ -38,7 +40,8 @@ switch ($implementation) {
     break;
     
     case 'file':
-        $repository = new AwsBootcamp\DataRepository\File('/tmp/test.json');
+        $repository = new AwsBootcamp\DataRepository\File($file);
+        $msg = 'Generated dataset file : ' . $file;
     break;
 
     default: 
@@ -49,6 +52,8 @@ switch ($implementation) {
 $gen = new AwsBootcamp\Generator\DataSet($repository);
 $config = require __DIR__ . '/../config/game-base.template.php';
 $dataSet = $gen->execute($config, 220, 50);
+
+echo PHP_EOL . $msg;
 
 echo PHP_EOL . 'Stats' . PHP_EOL;
 echo '---------' . PHP_EOL;
